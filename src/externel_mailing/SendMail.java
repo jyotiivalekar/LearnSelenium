16772c0bdd608a6b7dc2057ae993d81e4948efc1
Gaurav Singh <gauravsinghbr@hotmail.com>
2015-07-23 17:24:13 +0530

    updated SendMail.java

--- src/externel_mailing/SendMail.java
+++ src/externel_mailing/SendMail.java
@@ -11,41 +11,38 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
  
 public class SendMail {
+	
+	final String username = "username@gmail.com";
+	final String password = "password";
  
-	public static void main(String[] args) {
+	Properties props = new Properties();
+	props.put("mail.smtp.auth", "true");
+	props.put("mail.smtp.starttls.enable", "true");
+	props.put("mail.smtp.host", "smtp.gmail.com");
+	props.put("mail.smtp.port", "587");
  
-		final String username = "username@gmail.com";
-		final String password = "password";
+	Session session = Session.getInstance(props,
+	  new javax.mail.Authenticator() {
+		protected PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(username, password);
+		}
+	});
  
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+	try {
  
-		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
-			}
-		  });
- 
-		try {
- 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("from-email@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse("to-email@gmail.com"));
-			message.setSubject("Testing Subject");
-			message.setText("Dear Mail Crawler,"
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress("from-email@gmail.com"));
+		message.setRecipients(Message.RecipientType.TO,
+		InternetAddress.parse("to-email@gmail.com"));
+		message.setSubject("Testing Subject");
+		message.setText("Dear Mail Crawler,"
 				+ "\n\n No spam to my email, please!");
  
-			Transport.send(message);
+		Transport.send(message);
+ 		System.out.println("Done");
  
-			System.out.println("Done");
- 
-		} catch (MessagingException e) {
+	} catch (MessagingException e) {
 			throw new RuntimeException(e);
-		}
 	}
+
 }
